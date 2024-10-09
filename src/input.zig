fc8211225e6c7745ce680b14ca5786861ee16b5c
Roman <roman@volovoy.com>
2024-10-09 19:37:20 +0200

    handle cli input on linux

---
+++ src/input.zig
@@ -0,0 +1,90 @@
+const std = @import("std");
+const fs = std.fs;
+const linux = std.os.linux;
+
+pub const Command = enum { None, Quit, Select, Left, Down, Up, Right };
+
+pub const CliInput = struct {
+    tty_fd: fs.File.Handle = undefined,
+    old_settings: linux.termios = undefined,
+
+    pub fn init() !CliInput {
+        // source https://blog.fabrb.com/2024/capturing-input-in-real-time-zig-0-14/
+        const tty_file = try fs.openFileAbsolute("/dev/tty", .{});
+        const tty_fd = tty_file.handle;
+
+        var old_settings: linux.termios = undefined;
+        _ = linux.tcgetattr(tty_fd, &old_settings);
+
+        var new_settings: linux.termios = old_settings;
+        new_settings.lflag.ICANON = false;
+        new_settings.lflag.ECHO = false;
+
+        _ = linux.tcsetattr(tty_fd, linux.TCSA.NOW, &new_settings);
+
+        return .{ .tty_fd = tty_fd, .old_settings = old_settings };
+    }
+    pub fn deinit(self: CliInput) void {
+        _ = linux.tcsetattr(self.tty_fd, linux.TCSA.NOW, &self.old_settings);
+    }
+
+    pub fn getCommand(self: CliInput, reader: std.io.AnyReader) !Command {
+        _ = self;
+        const debug = std.debug;
+
+        var buffer: [3]u8 = undefined;
+        const n = try reader.read(buffer[0..1]);
+
+        if (n == 0) return Command.None; // Nothing read
+
+        const input = buffer[0];
+
+        switch (input) {
+            10, 32 => {
+                return Command.Select;
+            },
+
+            'q' => {
+                // try debug.print("Quitting...\n", .{});
+                return Command.Quit;
+            },
+            'h', 'j', 'k', 'l' => {
+                debug.print("Received '{}'\n", .{input});
+                // Handle hjkl movement
+                switch (input) {
+                    'h' => return Command.Left,
+                    'j' => return Command.Down,
+                    'k' => return Command.Up,
+                    'l' => return Command.Right,
+                    else => {
+                        debug.print("Unrecognized input: '{}'\n", .{input});
+                    },
+                }
+            },
+            // or use 27, thats escape
+            '\x1B' => {
+                // Handle escape sequences for arrow keys
+                const seq_read = try reader.read(buffer[1..2]);
+
+                if (seq_read != 0 and buffer[1] == '[') {
+                    _ = try reader.read(buffer[2..3]);
+
+                    switch (buffer[2]) {
+                        'A' => return Command.Up,
+                        'B' => return Command.Down,
+                        'C' => return Command.Right,
+                        'D' => return Command.Left,
+                        else => {
+                            debug.print("Unrecognized input: '{}'\n", .{buffer[2]});
+                        },
+                    }
+                }
+            },
+            else => {
+                debug.print("Unrecognized input: '{}'\n", .{input});
+            },
+        }
+
+        return Command.None;
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,19 @@
 const std = @import("std");
 const Board = @import("board.zig").Board;
-
+const Input = @import("input.zig");
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
     const stdout = std.io.getStdOut().writer().any();
-    // const stdin = std.io.getStdIn().reader();
+    const stdin = std.io.getStdIn().reader().any();
 
     var board = try Board.init(allocator, 3);
     defer board.deinit();
+
+    const input = try Input.CliInput.init();
+    defer input.deinit();
 
     try board.print(stdout);
     try stdout.writeAll("\n\n");
@@ -21,4 +24,25 @@ pub fn main() !void {
     try stdout.writeAll("\n\n");
 
     try stdout.print("Game state: {s}\n", .{@tagName(board.state())});
+
+    while (true) {
+        const cmd = try input.getCommand(stdin);
+
+        switch (cmd) {
+            Input.Command.Quit => {
+                try stdout.print("Quitting...\n", .{});
+                return;
+            },
+            Input.Command.Left, Input.Command.Down, Input.Command.Up, Input.Command.Right => {
+                // handle inputs
+            },
+            // TODO handle the rest
+            Input.Command.Select => {},
+            else => {
+                // std.debug.print("doint nothing", args: anytype)
+
+                std.debug.print("Got command {}\n", .{cmd});
+            },
+        }
+    }
 }
