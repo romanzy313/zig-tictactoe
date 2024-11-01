fa0a12075f33943f6662ebb9d9db5658b512b8a8
Roman <roman@volovoy.com>
2024-11-01 02:13:39 +0100

    trick or treat

--- src/learning/tips_and_tricks.zig
+++ src/learning/tips_and_tricks.zig
@@ -3,3 +3,18 @@
 
 // function initialization (at comptime)
 // pub fn init(pointer: anytype, comptime fillFn: fn (ptr: @TypeOf(pointer), buf: []u8) void) Random {
+
+// better example
+// pub fn sort(
+//     comptime T: type,
+//     items: []T,
+//     context: anytype,
+//     comptime lessThanFn: fn (@TypeOf(context), lhs: T, rhs: T) bool,
+// ) void {
+//     std.sort.block(T, items, context, lessThanFn);
+// }
+
+// initialize array of fixed size at comptime
+// comptime var routes = [_]RoutePart{.{
+//     .empty = {},
+// }} ** maxDepth;
